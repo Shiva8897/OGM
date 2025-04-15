c064bd3f9c5d44ba9260b5d2c1e067efa2e3ee87
Sai Vivek panchagiri <vivek.p@prudentconsulting.com>
2025-04-15 09:34:42 +0530

    Done User Registration

---
+++ OGM.Infra.sql/PlayCricketDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using OGM.Domain.Entities;
+
+namespace OGM.Infra.sql
+{
+    public class PlayCricketDbContext : DbContext
+    {
+        public PlayCricketDbContext(DbContextOptions<PlayCricketDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+
+}
---
+++ OGM.Repository/Interface/IUserRegistration.cs
@@ -0,0 +1,14 @@
+﻿using OGM.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OGM.Repository.Interface
+{
+    public interface IUserRegistration
+    {
+        Task<bool> RegisterUserAsync(User user);
+    }
+}
